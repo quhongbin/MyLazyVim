f923970aa2e3ae27e338d816564dc67484351b4d
Hongbin Qu <2818777520@qq.com>
2026-02-21 21:52:23 +0800

    set the images value of width and height in terminal

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -11,6 +11,10 @@ return {
     dependencies = { "luarocks.nvim" },
     opts = {
       processor = "magick_rock",
+      -- backend = "wezterm",
+      max_width = 30,
+      max_height = 30,
+      -- window_overlap_clear_enabled = true,
     },
   },
   --{
