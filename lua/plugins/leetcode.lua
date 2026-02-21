612265e44f0a0a7ef3ced2a47ba0dfcd8e2410ce
Hongbin Qu <2818777520@qq.com>
2026-02-21 21:54:15 +0800

    the leetcode plugin for solve the algorithms problems

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    opts = {
+      -- configuration goes here
+      cn = { -- leetcode.cn
+        enabled = true, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+      -- image_support = true,
+    },
+  },
+}
