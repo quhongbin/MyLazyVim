1f50d652b48a07d1ec94d951aa9329669c8af0b3
ubuntu24.04-qhb <2818777520@qq.com>
2026-04-16 14:19:30 +0800

    add lazyvim hererocks ,remove luarocks.nvim plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  rocks = {
+    -- image.nvim dependened
+    hererocks = true,
+  },
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,14 +1,6 @@
 return {
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1001, -- this plugin needs to run before anything else
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
     "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
     opts = {
       processor = "magick_rock",
       -- backend = "wezterm",
